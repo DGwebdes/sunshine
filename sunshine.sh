618be09f2019ee2f9e0ce27ce33c33224e09c888
DGwebdes <alan.bilik8@gmail.com>
2026-08-04 12:41:01 -0400

    cleaning up already, adding error handling;

--- sunshine.sh
+++ sunshine.sh
@@ -10,40 +10,53 @@ declare -A distro_pm=(
 	[fedora]="dnf"
 	[rhel]="dnf"
 	[arch]="pacman"
-	[suse]="zypper"
+	[opensuse]="zypper"
 )
 essentials=(build-essential libreadline-dev unzip)
 
 ## LUA commands and install
 install_lua(){
-	$(curl -L -R -O https://www.lua.org/ftp/lua-5.5.1.tar.gz) \
-	$(tar zxf lua-5.5.1.tar.gz) \
-	$(cd lua-5.5.1) \
-	$(make all test) \
-	$(cd ..) \
-	$(rm -rf lua-*)
+	curl -L -R -O https://www.lua.org/ftp/lua-5.5.1.tar.gz \
+	tar zxf lua-5.5.1.tar.gz \
+	cd lua-5.5.1 \
+	make all test \
+	cd .. \
+	rm -rf lua-*
 }
 ## LuaRocks
 install_lr(){
-	$(wget https://luarocks.org/releases/luarocks-3.13.0.tar.gz) \
-	$(tar zxpf luarocks-3.13.0.tar.gz) \
-	$(cd luarocks-3.13.0) \
-	$(./configure && make && sudo make install) \
-	$(sudo luarocks install luasocket) \
-	$(cd ..) \
-	$(rm -rf luarocks*)
+	wget https://luarocks.org/releases/luarocks-3.13.0.tar.gz \
+	tar zxpf luarocks-3.13.0.tar.gz \
+	cd luarocks-3.13.0 \
+	./configure && make && sudo make install \
+	sudo luarocks install luasocket \
+	cd .. \
+	rm -rf luarocks*
 }
 ## Neovim
-instal_nvim(){
-	$(curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz) \
-	$(sudo rm -rf /opt/nvim-linux-x86_64) \
-	$(sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz) \
-	$(export PATH="$PATH:/opt/nvim-linux-x86_64/bin" >> .bashrc)
+install_nvim(){
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+	if [[ $?  -ne 0 ]]; then
+		echo "Error curling"
+	fi
+	sudo rm -rf /opt/nvim-linux-x86_64
+	if [[ $?  -ne 0 ]]; then
+		echo "Error removing directory"
+	fi
+	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+	if [[ $?  -ne 0 ]]; then
+		echo "Error unzipping"
+	fi
+	echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> $HOME/.bashrc
+	if [[ $?  -ne 0 ]]; then
+		echo "Error saving to PATH"
+	fi
+	echo "Neovim Installed"
 }
 ## nvim Kickstart
 install_kick(){
-	sudo $1 install ripgreg fd-find xclip tree-sitter-cli
-	if [[ $? > 1 ]]; then
+	sudo $1 install ripgrep fd-find xclip tree-sitter-cli
+	if [[ $?  -ne 0 ]]; then
 		echo "something went wrong with the previous step"
 	else
 		echo "cool, we can kickstart now"
@@ -95,10 +108,10 @@ installer(){
 	
 	echo $inst_comm
 
-	#sudo $pm $inst_comm
+	#sudo "$pm" $inst_comm
 
 	#for i in ${essentials[@]}; do
-	#	sudo $pm install $i
+	#	sudo "$pm" install "$i"
 	#done
 
 	echo "Installing Lua"
