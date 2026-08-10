0cebb72f4bb7a1c588fe5ae05ff287d99cd6bb36
DGwebdes <alan.bilik8@gmail.com>
2026-08-10 13:11:11 -0400

    redirecting output to install.log file instead of the terminal

--- sunshine.sh
+++ sunshine.sh
@@ -22,37 +22,37 @@ error_handler(){
 
 ## LUA commands and install
 install_lua(){
-	curl -L -R -O https://www.lua.org/ftp/lua-5.5.1.tar.gz || error_handler "Curl failed to Download Lua" 2 
-	tar zxf lua-5.5.1.tar.gz || error_handler "Tar failed to extract" 2
-	cd lua-5.5.1 || error_handler "Failed to cd into lua directory" 2
-	make all test || error_handler "Failed to build Lua" 2
-	sudo make install || error_handler "Failed to Install Lua" 2
+	curl -L -R -O https://www.lua.org/ftp/lua-5.5.1.tar.gz >> install.log 2>&1 || error_handler "Curl failed to Download Lua" 2
+	tar zxf lua-5.5.1.tar.gz >> install.log 2>&1 || error_handler "Tar failed to extract" 2
+	cd lua-5.5.1 >> install.log 2>&1 || error_handler "Failed to cd into lua directory" 2
+	make all test >> install.log 2>&1 || error_handler "Failed to build Lua" 2
+	sudo make install >> install.log 2>&1 || error_handler "Failed to Install Lua" 2
 	echo "Cleaning up"
 	cd .. 
 	rm -rf lua-*
 }
 ## LuaRocks
 install_lr(){
-	wget https://luarocks.org/releases/luarocks-3.13.0.tar.gz || error_handler "Wget failed to download luarocks" 2
-	tar zxpf luarocks-3.13.0.tar.gz || error_handler "Tar failed to extract" 2
-	cd luarocks-3.13.0 || error_handler "Failed to cd into LuaRocks" 2
-	./configure --with-lua-include=/usr/local/include && make && sudo make install || error_handler "Failed to build LuaRocks" 2
-	sudo luarocks install luasocket  || error_handler "Failed to install LuaRocks" 2
+	wget https://luarocks.org/releases/luarocks-3.13.0.tar.gz >> install.log 2>&1 || error_handler "Wget failed to download luarocks" 2
+	tar zxpf luarocks-3.13.0.tar.gz >> install.log 2>&1 || error_handler "Tar failed to extract" 2
+	cd luarocks-3.13.0 >> install.log 2>&1 || error_handler "Failed to cd into LuaRocks" 2
+	./configure --with-lua-include=/usr/local/include && make && sudo make install >> install.log 2>&1 || error_handler "Failed to build LuaRocks" 2
+	sudo luarocks install luasocket  >> install.log 2>&1 || error_handler "Failed to install LuaRocks" 2
 	echo "Cleaning up"
 	cd .. 
 	rm -rf luarocks*
 }
 ## Neovim
 install_nvim(){
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz || error_handler "Curl error to fetch neovim tarball" 2
-	sudo rm -rf /opt/nvim-linux-x86_64 || error_handler "Could not delete directory" 2
-	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz || error_handler "Tar extraction failed" 2
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz >> install.log 2>&1 || error_handler "Curl error to fetch neovim tarball" 2
+	sudo rm -rf /opt/nvim-linux-x86_64 >> install.log 2>&1 || error_handler "Could not delete directory" 2
+	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz >> install.log 2>&1 || error_handler "Tar extraction failed" 2
 	rm -rf nvim-*
 	echo "Neovim Installed"
 }
 ## nvim Kickstart
 install_kick(){
-	sudo $1 $2 $3 ripgrep fd-find xclip tree-sitter-cli || error_handler "Failed to install kickstart essentials" 2
+	sudo $1 $2 $3 ripgrep fd-find xclip tree-sitter-cli >> install.log 2>&1 || error_handler "Failed to install kickstart essentials" 2
 	git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 }
 
@@ -60,7 +60,7 @@ install_kick(){
 zsh_default(){
 	if ! command -v zsh >/dev/null 2>&1; then
 		echo "Could not find zsh. Installing..."
-		sudo $1 $2 $3 zsh || error_handler "Could not install zsh" 2
+		sudo $1 $2 $3 zsh >> install.log 2>&1 || error_handler "Could not install zsh" 2
 		chsh -s $(command -v zsh)
 	else
 		echo "Seems like you already have it!"
@@ -69,7 +69,7 @@ zsh_default(){
 
 	## Install oh-my-zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-	echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> $HOME/.zshrc || error_handler "failed to export to path" 2
+	echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> $HOME/.zshrc >> install.log 2>&1 || error_handler "failed to export to path" 2
 	zsh
 }
 
