ba08f47cd2f448f30af1299d5158c68714d4b07e
DGwebdes <alan.bilik8@gmail.com>
2026-08-28 13:42:53 -0400

    running zsh AFTER omz script has finished. logs collected in one directory

--- sunshine.sh
+++ sunshine.sh
@@ -34,22 +34,22 @@ error_handler(){
 
 ## LUA commands and install
 install_lua(){
-	curl -L -R -O https://www.lua.org/ftp/lua-5.5.1.tar.gz >> install.log 2>&1 || error_handler "Curl failed to Download Lua" 2
-	tar zxf lua-5.5.1.tar.gz >> install.log 2>&1 || error_handler "Tar failed to extract" 2
-	cd lua-5.5.1 >> install.log 2>&1 || error_handler "Failed to cd into lua directory" 2
-	make all test >> install.log 2>&1 || error_handler "Failed to build Lua" 2
-	sudo make install >> install.log 2>&1 || error_handler "Failed to Install Lua" 2
+	curl -L -R -O https://www.lua.org/ftp/lua-5.5.1.tar.gz >> lua_install.log 2>&1 || error_handler "Curl failed to Download Lua" 2
+	tar zxf lua-5.5.1.tar.gz >> lua_install.log 2>&1 || error_handler "Tar failed to extract" 2
+	cd lua-5.5.1 >> lua_install.log 2>&1 || error_handler "Failed to cd into lua directory" 2
+	make all test >> lua_install.log 2>&1 || error_handler "Failed to build Lua" 2
+	sudo make install >> lua_install.log 2>&1 || error_handler "Failed to Install Lua" 2
 	job_done "Cleaning up"
 	cd .. 
 	rm -rf lua-*
 }
 ## LuaRocks
 install_lr(){
-	wget https://luarocks.org/releases/luarocks-3.13.0.tar.gz >> install.log 2>&1 || error_handler "Wget failed to download luarocks" 2
-	tar zxpf luarocks-3.13.0.tar.gz >> install.log 2>&1 || error_handler "Tar failed to extract" 2
-	cd luarocks-3.13.0 >> install.log 2>&1 || error_handler "Failed to cd into LuaRocks" 2
-	./configure --with-lua-include=/usr/local/include >> install.log 2>&1 && make >> install.log 2>&1 && sudo make install >> install.log 2>&1 || error_handler "Failed to build LuaRocks" 2
-	sudo luarocks install luasocket  >> install.log 2>&1 || error_handler "Failed to install LuaRocks" 2
+	wget https://luarocks.org/releases/luarocks-3.13.0.tar.gz >> lr_install.log 2>&1 || error_handler "Wget failed to download luarocks" 2
+	tar zxpf luarocks-3.13.0.tar.gz >> lr_install.log 2>&1 || error_handler "Tar failed to extract" 2
+	cd luarocks-3.13.0 >> lr_install.log 2>&1 || error_handler "Failed to cd into LuaRocks" 2
+	./configure --with-lua-include=/usr/local/include >> lr_install.log 2>&1 && make >> lr_install.log 2>&1 && sudo make install >> lr_install.log 2>&1 || error_handler "Failed to build LuaRocks" 2
+	sudo luarocks install luasocket  >> lr_install.log 2>&1 || error_handler "Failed to install LuaRocks" 2
 	job_done "Cleaning up"
 	cd .. 
 	rm -rf luarocks*
@@ -57,8 +57,8 @@ install_lr(){
 ## Neovim
 install_nvim(){
 	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz >> install.log 2>&1 || error_handler "Curl error to fetch neovim tarball" 2
-	sudo rm -rf /opt/nvim-linux-x86_64 >> install.log 2>&1 || error_handler "Could not delete directory" 2
-	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz >> install.log 2>&1 || error_handler "Tar extraction failed" 2
+	sudo rm -rf /opt/nvim-linux-x86_64 >> nvim_install.log 2>&1 || error_handler "Could not delete directory" 2
+	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz >> nvim_install.log 2>&1 || error_handler "Tar extraction failed" 2
 	job_done "Cleaning up"
 	rm -rf nvim-*
 }
@@ -71,7 +71,7 @@ install_kick(){
 zsh_default(){
 	if ! command -v zsh >/dev/null 2>&1; then
 		info_handler "Could not find zsh. Installing..."
-		sudo "$1" "${@:2}" zsh >> install.log 2>&1 || error_handler "Could not install zsh" 2
+		sudo "$1" "${@:2}" zsh >> zsh_install.log 2>&1 || error_handler "Could not install zsh" 2
 		sudo chsh -s $(command -v zsh) $USER
 	else
 		info_handler "Seems like you already have it!"
@@ -79,11 +79,17 @@ zsh_default(){
 	fi
 
 	## Install oh-my-zsh
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || error_handler "Failed to install oh-my-zsh" 2
+	RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >> omz.log 2>&1 || error_handler "Failed to install oh-my-zsh" 2
 
 	# Export neovim to PATH after oh-my-zsh is installed and zshrc file is set
 	echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> $HOME/.zshrc || error_handler "failed to export to path" 2
-	source $HOME/.zshrc || error_handler "Failed to source .zshrc file" 2
+
+	# Collecting logs into one directory
+	job_done "Tidying up the room..."
+	mkdir -p sunshine-logs && mv ./*.log sunshine-logs
+
+	# spawning zsh Shell
+	exec zsh
 }
 
 
